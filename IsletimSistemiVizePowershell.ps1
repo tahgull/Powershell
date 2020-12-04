3eef7ff691ce0516b14748cc9148da499dc61b5a
= <==>
2020-12-05 00:31:48 +0300

    Powershell Vize

---
+++ IsletimSistemiVizePowershell.ps1
@@ -0,0 +1,19 @@
+﻿ Get-Service | Where-Object {$_.DependentServices} |
+    Format-List -Property Name, DependentServices, @{
+      Label="Services"; Expression={$_.dependentservices.count}
+    }
+
+     Get-NetTCPConnection -State Listen |
+        Select-Object -Property  LocalAddress,
+                                 LocalPort,
+                                 RemoteAddress,
+                                 RemotePort,
+                                @{name='Process';expression={(Get-Process -Id $_.OwningProcess).Name}},
+                                CreationTime |
+        Format-Table -AutoSize
+
+$output += Get-NetTCPConnection 
+
+
+$output | Out-File -FilePath./output.txt
+   
